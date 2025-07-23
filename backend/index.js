61f25fcb9244697d19d245c4247bf3925a47d9cb
Chaitra-t-shetty <127846751+Chaitra-t-shetty@users.noreply.github.com>
2025-07-23 23:26:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://rural-connect-frontend.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
