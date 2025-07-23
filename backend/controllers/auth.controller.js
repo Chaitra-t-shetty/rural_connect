50ee497ca3d97acc204f084f149058b88edf3cc8
Chaitra-t-shetty <127846751+Chaitra-t-shetty@users.noreply.github.com>
2025-07-24 00:22:53 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -34,7 +34,7 @@ export const signUp = async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite:"strict",
+            sameSite:"None",
             secure:process.env.NODE_ENVIRONMENT==="production"
         })
         return res.status(201).json(user)
